93d70aba07da4fb8415132f6ef95d704abd81bf1
unknown <rojandhimal1@gmail.com>
2021-04-04 17:22:32 +0545

    securing token private request

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const ErrorResponse = require("../utils/errorResponse");
 
 exports.register = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -9,15 +10,9 @@ exports.register = async (req, res, next) => {
       email,
       password,
     });
-    res.status(201).json({
-      success: true,
-      user,
-    });
+    sendToken(user, 201, res);
   } catch (error) {
-    res.status(500).json({
-      success: true,
-      error: error.message,
-    });
+    next(error);
   }
 };
 
@@ -25,28 +20,23 @@ exports.login = async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    res
-      .status(400)
-      .json({ success: false, error: "Please provide email and password" });
+   return next(new ErrorResponse("Please provide an email and password", 400));
   }
   try {
     const user = await User.findOne({ email }).select("+password");
     if (!user) {
-      res.status(404).json({ success: false, error: "Invalid credintials" });
+      return next(new ErrorResponse("Invalid credintials", 404));
     }
 
     const isMatch = await user.matchPasswords(password);
 
     if (!isMatch) {
-      res.status(404).json({ success: false, error: "Invalid credintials" });
+      return next(new ErrorResponse("Invalid credintials", 401));
     }
 
-    res.status(200).json({
-      success: true,
-      token: "tergeasd1442",
-    });
+    sendToken(user, 200, res);
   } catch (error) {
-    res.status(500).json({
+    res.status(500).json({  
       success: false, 
       error: error.message,
     });
@@ -59,4 +49,13 @@ exports.forgotPassword = (req, res, next) => {
 
 exports.resetPassword = (req, res, next) => {
   res.send("Reset password route");
+};
+
+
+const sendToken = (user, statusCode, res) => {
+  const token = user.getSignedJwtToken();
+  res.status(statusCode).json({
+    success: true,
+    token: token,
+  });
 };
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require("dotenv").config({ path: "./config.env" });
 const express = require("express");
 const connectDB = require("./config/db");
 
+const errorHandler = require("./middleware/error");
+
 //connect DB
 connectDB();
 
@@ -10,6 +12,10 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/Auth"));
+app.use("/api/private", require("./routes/private"));
+
+//Error Handler should be last piece of middleware
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
